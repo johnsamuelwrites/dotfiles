858142ca294ff414d3d80b74e91063719b0f4e59
John Samuel <johnsamuelwrites@gmail.com>
2020-08-17 10:31:56 +0200

    Update functions for memory, network and process

--- bash/en/functions.sh
+++ bash/en/functions.sh
@@ -29,13 +29,51 @@ function deleteaction() {
   then
     shift
     rm $@
+  elif [[ $1 == "process" ]]
+  then
+    shift
+    kill -9 $@
   elif [[ $1 == "directory" ]]
   then
     shift
     rmdir $@
   fi
 }
+function listaction() {
+  count=$#
+  if [[ $1 == "file" ]]
+  then
+    shift
+    ls -a $@
+  elif [[ $1 == "directory" ]]
+  then
+    shift
+    ls $@
+  elif [[ $1 == "network" ]]
+  then
+    shift
+    netstat -natp 
+  elif [[ $1 == "cpu" ]]
+  then
+    shift
+    lscpu 
+  elif [[ $1 == "hardware" ]]
+  then
+    shift
+    lshw
+  elif [[ $1 == "memory" ]]
+  then
+    shift
+    cat /proc/meminfo
+  elif [[ $1 == "process" ]]
+  then
+    shift
+    ps -aux
+  fi
+}
 
 alias create="createaction"
 alias read="readaction"
 alias delete="deleteaction"
+alias list="listaction"
+alias show="list"
