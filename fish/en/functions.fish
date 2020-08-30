17d331d1f93c6b0acc94ff2318bb04fda174ee45
John Samuel <johnsamuelwrites@gmail.com>
2020-08-30 14:44:23 +0200

    Add support for fish commands: delete list

--- fish/en/functions.fish
+++ fish/en/functions.fish
@@ -16,17 +16,52 @@ function show --description 'read or show file or directory'
   end
 end
 
+function delete --description 'delete file, process or directory'
+  set argcount (count $argv)
+  if test $argv[1] = "file"
+    rm $argv[2..$argcount]
+  else if test $argv[1] = "process" 
+    kill -9 $argv[2..$argcount]
+  else if test $argv[1] = "directory" 
+    rmdir $argv[2..$argcount]
+  end
+end
+
+function list --description 'list file, process or directory'
+  set argcount (count $argv)
+  if test $argv[1] = "file"
+    ls $argv[2..$argcount]
+  else if test $argv[1] = "process" 
+    ps -aux
+  else if test $argv[1] = "directory" 
+    ls
+  else if test $argv[1] = "network" 
+    netstat -natp
+  else if test $argv[1] = "cpu" 
+    lscpu
+  else if test $argv[1] = "hardware" 
+    lshw
+  else if test $argv[1] = "memory" 
+    cat /proc/meminfo
+  end
+end
+
 # Add support for autocompletion of options
 # https://stackoverflow.com/questions/16657803/creating-autocomplete-script-with-sub-commands
 function functions_with_options
   set cmd (commandline -opc)
-  if [ (count $cmd) -eq 1 -a $cmd[1] = 'show' -o $cmd[1] = 'create' ]
-    return 0
+  set commands "show" "create" "delete" "list"
+  if [ (count $cmd) -eq 1 ]
+    if contains $cmd[1] $commands
+      return 0
+    end
   end
   return 1
 end
 
-funcsave create show
+funcsave create show delete list
 funcsave functions_with_options
 complete -f -c create -n 'functions_with_options' -a 'file directory'
 complete -f -c show -n 'functions_with_options' -a 'file directory'
+complete -f -c delete -n 'functions_with_options' -a 'file directory process'
+complete -f -c list -n 'functions_with_options' -a 'file directory process network cpu hardware'
